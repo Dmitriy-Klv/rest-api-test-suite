aaebbfb8716ab8f2414fac480c9bba4b3cbc200c
Dmitriy-Klv <dmklvd@gmail.com>
2026-02-05 16:04:07 +0100

    fix test_data_validation

--- tests/posts/test_get_posts_data_validation.py
+++ tests/posts/test_get_posts_data_validation.py
@@ -6,6 +6,6 @@ def test_post_data_validation(valid_post, post_id):
     post = valid_post(post_id)
 
     assert post.id > 0
-    assert post.userId > 0
+    assert post.user_id > 0
     assert post.title.strip() != ""
     assert post.body.strip() != ""
