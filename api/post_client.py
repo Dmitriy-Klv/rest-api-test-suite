02346beb49fd3ee5cccb366d29890e2971083e0e
Dmitriy-Klv <dmklvd@gmail.com>
2026-02-11 09:31:41 +0100

    add CRUD methods

--- api/post_client.py
+++ api/post_client.py
@@ -13,3 +13,12 @@ class PostClient(BaseAPI):
 
     def create_post(self, payload: dict):
         return self.post(self.endpoint, json=payload)
+
+    def update_post(self, post_id: int, payload: dict):
+        return self.put(f"{self.endpoint}/{post_id}", json=payload)
+
+    def patch_post(self, post_id: int, payload: dict):
+        return self.patch(f"{self.endpoint}/{post_id}", json=payload)
+
+    def delete_post(self, post_id: int):
+        return self.delete(f"{self.endpoint}/{post_id}")
